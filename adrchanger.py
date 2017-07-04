113f5da4ee23c8c1d8ae12e7130f15796e4e59c4
Max Brüggemann <mail@maxbrueggemann.de>
2017-07-04 23:57:21 +0200

    Added address change script

---
+++ adrchanger.py
@@ -0,0 +1,53 @@
+# Ugly code following!     
+
+import array
+import pymodbus
+import serial
+import argparse
+from pymodbus.pdu import ModbusRequest
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient #initialize a serial RTU client instance
+from pymodbus.transaction import ModbusRtuFramer
+
+#import logging
+#logging.basicConfig()
+#log = logging.getLogger()
+#log.setLevel(logging.DEBUG)
+
+baudrate = 38400
+parity = 'N'
+port = "/dev/ttyUSB0"
+
+global newAdr
+
+
+def chAdr():
+
+	client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate)
+	try:
+		connection = client.connect()
+		adrReg = client.read_holding_registers(1000,1,unit=unit)
+		result = client.write_register(1000,newAdr,unit=unit)		
+		adrReg = client.read_holding_registers(1000,1,unit=unit)
+		if adrReg.registers[0] == newAdr:
+			print("Write: ok")
+			result = client.write_register(1001,16727,unit=unit)		
+		print("Store: ok")
+	except:
+		print("Failed!")
+	try:
+		adrReg = client.read_holding_registers(1000,1,unit=newAdr)
+		print("Verification: pass")
+		print("New address: "+str(newAdr))
+	except:
+		print("Verification failed!")
+	client.close()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("address", type=int, help="device's current address")
+parser.add_argument("newAddress", type=int, help="new device address")
+args = parser.parse_args()
+unit = args.address
+newAdr = args.newAddress
+chAdr()
+

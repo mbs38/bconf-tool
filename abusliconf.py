75bf2ad9e511902dd47031f7e01b4f331ad10c81
Max Brüggemann <mail@maxbrueggemann.de>
2017-07-04 19:29:49 +0200

    Init repo.

---
+++ abusliconf.py
@@ -0,0 +1,276 @@
+import ConfigParser
+
+
+outputList=[]
+sectionList=[]
+for x in range(0,16):
+	sectionList.append("Input"+str(x))
+	outputList.append("Output"+str(x))
+#rint(sectionList)
+	
+
+config = ConfigParser.RawConfigParser()
+
+
+def makeEmptyConfFile():
+	global filename
+	config.add_section('Global')
+	config.set('Global', 'timeout', 20)
+	config.set('Global', 'long-push-threshold','')
+	for section in sectionList:
+		config.add_section(section)
+		config.set(section, 'short-on', '')
+		config.set(section, 'short-off','')
+		config.set(section, 'long-on','')
+		config.set(section, 'long-off','')
+		config.set(section, 'switch-type', 'push-button')
+	for outputsection in outputList:
+		config.add_section(outputsection)
+		config.set(outputsection, 'input-controlled','never')
+
+# Writing our configuration file to 'example.cfg'
+	with open(filename, 'wb') as configfile:
+    		config.write(configfile)
+
+
+#makeEmptyConfFile()
+#config = ConfigParser.RawConfigParser()
+
+buttonConf=[False]*16
+ioConf=[False]*1024
+oConf=[False]*32
+global timeoutThr
+global longPushThr
+longPushThr=0
+timeoutThr=0
+
+def readConfFromFile():
+	global filename
+	config.read(filename)
+	try:
+		longPushThr = int(config.get('Global', 'long-push-threshold'))
+		#print("Long-push-thr: "+str(longPushThr))
+	except:
+		longPushThr = 30
+		#print("no long push threshold in config file")
+	try:	
+		timeoutThr = int(config.get('Global', 'timeout'))
+		#print("Timeout: "+str(timeoutThr))
+	except:
+		timeoutThr = 0
+		#print("no timeout thresold in config file")
+
+	for sections in sectionList:
+		try:
+			shortOn = map(int,config.get(sections, 'short-on').split(','))
+		#	print("Section: "+str(sectionList.index(sections))+", shortOn: "+str(shortOn))
+		except:
+			shortOn=[]
+		try:
+			shortOff = map(int,config.get(sections, 'short-off').split(','))
+		#	print("Section: "+str(sectionList.index(sections))+", shortOff: "+str(shortOff))
+		except:
+			shortOff=[]
+		try:
+			longOn = map(int,config.get(sections, 'long-on').split(','))
+		#	print("Section: "+str(sectionList.index(sections))+", longOn: "+str(longOn))
+		except:
+			longOn=[]
+		try:
+			longOff = map(int,config.get(sections, 'long-off').split(','))
+		#	print("Section: "+str(sectionList.index(sections))+", longOff: "+str(longOff))
+		except:
+			longOff=[]
+		try:
+			switchType = config.get(sections, 'switch-type')
+		except:
+			switchType = "push-button"
+
+		########################### Arrange data, copy into boolean list ###############################
+		if sectionList.index(sections)>7:
+			skip = 512
+			multiplier=(sectionList.index(sections)-8)*8
+		else:
+			skip = 0
+			multiplier=(sectionList.index(sections))*8
+
+		for x in xrange(len(shortOn)):
+			if (shortOn[x] < 8):
+				bitLocation=shortOn[x]+multiplier+skip
+				ioConf[bitLocation]=True	
+			else:
+				if(shortOn[x] > 15):
+					raise Exception("Output doesn't exist")
+				bitLocation=shortOn[x]+multiplier+skip+128-8
+				ioConf[bitLocation]=True
+		#	print(str(shortOn[x])+": Bit"+str(bitLocation))
+		
+		for x in xrange(len(shortOff)):
+			if (shortOff[x] < 8):
+				bitLocation=shortOff[x]+multiplier+64+skip
+				ioConf[bitLocation]=True	
+			else:
+				bitLocation=shortOff[x]+multiplier+64+128+skip-8
+				ioConf[bitLocation]=True
+		#	print(str(shortOff[x])+": Bit"+str(bitLocation))
+		
+
+		for x in xrange(len(longOn)):
+			if (longOn[x] < 8):
+				bitLocation=longOn[x]+256+multiplier+skip
+				ioConf[bitLocation]=True	
+			else:
+				if(longOn[x] > 15):
+					raise Exception("Output doesn't exist")
+				bitLocation=longOn[x]+256+multiplier+skip+128-8
+				ioConf[bitLocation]=True
+		#	print(str(longOn[x])+": Bit"+str(bitLocation))
+
+		for x in xrange(len(longOff)):
+			if (longOff[x] < 8):
+				bitLocation=longOff[x]+256+64+multiplier+skip
+				ioConf[bitLocation]=True	
+			else:
+				if(longOff[x] > 15):
+					raise Exception("Output doesn't exist")
+				bitLocation=longOff[x]+256+multiplier+64+skip+128-8
+				ioConf[bitLocation]=True
+		#	print(str(longOff[x])+": Bit"+str(bitLocation))
+		
+		if switchType == "toggle-switch":
+		#	print("Input"+str(sectionList.index(sections))+": Schalter")
+			buttonConf[sectionList.index(sections)]=True
+		#else:
+		#	print("Input"+str(sectionList.index(sections))+": Taster")
+		
+	for outputsection in outputList:
+		try:
+			oConfStr = config.get(outputsection, 'input-controlled')
+		except:
+			oConfStr=[]
+		if oConfStr == 'timeout-only':
+			oConf[outputList.index(outputsection)+16]=True
+		elif oConfStr == 'always':
+			oConf[outputList.index(outputsection)]=True
+			oConf[outputList.index(outputsection)+16]=True
+
+########################################################################################
+#@brief: writeConfTofile(), write configuration data to file
+def writeConfToFile():
+	global filename
+	configwriter = ConfigParser.RawConfigParser()
+	configwriter.add_section('Global')
+	configwriter.set('Global', 'timeout', timeoutThr)
+	configwriter.set('Global', 'long-push-threshold', longPushThr)
+	for section in sectionList:
+		try:
+			configwriter.add_section(section)
+		except:
+			print("Section "+section+" already exists, overwriting")
+####################### make dummy config file ##########################################
+		configwriter.set(section, 'short-on', '')
+		configwriter.set(section, 'short-off','')
+		configwriter.set(section, 'long-on','')
+		configwriter.set(section, 'long-off','')
+		configwriter.set(section, 'switch-type', 'push-button')
+	for outputsection in outputList:
+		try:
+			configwriter.add_section(outputsection)
+		except:
+			print("Section "+outputsection+" already exists, overwriting")
+		configwriter.set(outputsection, 'input-controlled','never')
+####################### prepare lists for config file #####################################
+
+	for x in range(0,16):
+		if(buttonConf[x]==True):
+			configwriter.set(sectionList[x], 'switch-type', 'toggle-switch')
+	
+
+
+
+
+	#for x in range(0,1024):
+		#if x<64:
+		#	if ioConf[x]==True:
+							
+		##	print("i0to7: son"+str(x/8)+sectionList[x/8])
+		#if x>63 and x<128:
+		#	print("i0to7: soff")
+		
+			
+	
+	for section in sectionList:
+		shortOn = []
+		shortOff = []
+		longOn = []
+		longOff = []
+		
+		
+		if sectionList.index(section)>7:
+			skip = 512
+			multiplier=(sectionList.index(section)-8)*8
+		else:
+			skip = 0
+			multiplier=(sectionList.index(section))*8
+		
+		#shorton:
+		for x in range(0,16):
+			if x<8  and ioConf[x+multiplier+skip]==True:
+				shortOn.append(x)
+				
+			if x>7 and ioConf[x-8+multiplier+skip+128]==True:
+				shortOn.append(x)
+			
+			
+			if x<8 and ioConf[x+64+multiplier+skip]==True:
+				shortOff.append(x)
+			if x>7 and ioConf[x-8+64+128+multiplier+skip]==True:
+				shortOff.append(x)
+
+			
+			if x<8 and ioConf[x+256+multiplier+skip]==True:
+				longOn.append(x)
+			if x>7 and ioConf[x-8+256+128+multiplier+skip]==True:
+				longOn.append(x)
+
+		
+			if x<8 and ioConf[x+256+64+multiplier+skip]==True:
+				longOff.append(x)
+			if x>7 and ioConf[x-8+256+64+128+multiplier+skip]==True:
+				longOff.append(x)
+
+
+		
+		
+		#shortOn = map(int,config.get(sections, 'short-on').split(','))
+		#print("Section: "+str(sectionList.index(sections))+", shortOn: "+str(shortOn))
+	#	shortOff = map(int,config.get(sections, 'short-off').split(','))
+	#		print("Section: "+str(sectionList.index(sections))+", shortOff: "+str(shortOff))
+	#	except:
+######################### finally, write to file ##################################
+		configwriter.set(section, 'short-on', ','.join(map(str,shortOn)))
+		configwriter.set(section, 'short-off', ','.join(map(str,shortOff)))
+		configwriter.set(section, 'long-on', ','.join(map(str,longOn)))
+		configwriter.set(section, 'long-off', ','.join(map(str,longOff)))
+		
+	for outputsection in outputList:
+		if(oConf[outputList.index(outputsection)+16]):
+			configwriter.set(outputsection, 'input-controlled','timeout-only')
+		if(oConf[outputList.index(outputsection)]):
+			configwriter.set(outputsection, 'input-controlled','always')
+	with open(filename, 'wb') as configfile:
+    		configwriter.write(configfile)
+
+
+#makeEmptyConfFile()
+#readConfFromFile()
+#writeConfToFile()
+#blahconfiwas=[False]*1024
+#blahconfiwas[1000]=True
+#if ( blahconfiwas[1000]==True ):
+#	print("Es ist wahr!")
+#print(blahconfiwas)
+
+#for x in xrange(len(ioConf)):
+#	if ioConf[x]==True:
+#		print("Bit" + str(x) + ": " + str(ioConf[x]))
---
+++ bconf-tool.py
@@ -0,0 +1,164 @@
+# Ugly code following!     
+
+import abusliconf
+import array
+import pymodbus
+import serial
+import argparse
+from pymodbus.pdu import ModbusRequest
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient #initialize a serial RTU client instance
+from pymodbus.transaction import ModbusRtuFramer
+
+#import logging
+#logging.basicConfig()
+##log = logging.getLogger()
+#log.setLevel(logging.DEBUG)
+
+
+#count= the number of registers to read
+baudrate = 38400
+parity = 'N'
+port = "/dev/ttyUSB0"
+unit=13 # the slave unit this request is targeting
+#address= the starting address to read from
+
+
+IOcffromDevice = [False]*1024
+buttonConf = [False]*16
+oConfFromDevice = [False]*32
+cmdRegisters = [0]*3
+#afor x in range(0, 1023):
+#	IOcffromDevice[x]=False
+
+def readConfs():
+
+	client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate)
+	#client = ModbusClient("rtu", "/dev/ttyusB0", 1, 8, 'N', 38400)
+	try:
+		connection = client.connect()
+		result0 = client.read_coils(2000,512,unit=unit)
+		result1 = client.read_coils(2511,512,unit=unit)
+		result2 = client.read_coils(3024,32,unit=unit)
+		result3 = client.read_coils(3056,16,unit=unit)
+		result4 = client.read_holding_registers(2000,3,unit=unit)
+		
+		cmdRegisters[1]=int(result4.registers[1])
+		cmdRegisters[2]=int(result4.registers[2])
+		for x in range(0, 512):
+			IOcffromDevice[x] = result0.bits[x]
+			IOcffromDevice[x+512] = result1.bits[x]
+		for x in range(0, 16):
+			buttonConf[x]=result3.bits[x]
+		for x in range(0, 32):
+			oConfFromDevice[x]=result2.bits[x]
+	except:
+		print("Modbus error.")
+	client.close()
+
+def compare():
+
+	print("Downloading from device "+str(unit))
+	readConfs()
+	abusliconf.readConfFromFile()
+	testResult = 0
+	if(IOcffromDevice==abusliconf.ioConf):
+		print("input-output-assignments match.")
+	else:
+		testResult=1
+		print("input-output-assignments dont match!")
+	if(buttonConf==abusliconf.buttonConf):
+		print("button configuration matches")
+	else:	
+		testResult=1
+		print("button configuration doesn't match!")
+	if(oConfFromDevice==abusliconf.oConf):
+		print("output configuration matches.")
+	else:
+		testResult=1
+		print("output configuration doesn't match!")
+	if(abusliconf.longPushThr==cmdRegisters[1]):
+		print("longpush threshold matches.")
+	else:
+		testResult=1
+		print("longpush threshold doesn't match!")
+	if(abusliconf.timeoutThr==cmdRegisters[2]):
+		print("bus timeout threshold matches.")
+	else:
+		print("bus timeout threshold doesn't match!")
+		testResult=1
+
+	if testResult == 1:
+		print("ERROR: The device's configuration doesn't match the config file!")
+		return False
+	else:
+		return True
+
+def store():
+	client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate)
+	try:
+		connection = client.connect()
+		result4 = client.write_register(2000,17239,unit=unit)
+		print("Store: ok")
+	except:
+		print("Modbus error.")
+	client.close()
+
+
+def upload():
+	abusliconf.readConfFromFile()
+	
+	client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate)
+	try:
+		connection = client.connect()
+		result0 = client.write_coils(2000,abusliconf.ioConf[0:512],unit=unit)
+		result0 = client.write_coils(2512,abusliconf.ioConf[512:1024],unit=unit)
+		result2 = client.write_coils(3024,abusliconf.oConf,unit=unit)
+		result3 = client.write_coils(3056,abusliconf.buttonConf,unit=unit)
+		result4 = client.write_register(2001,abusliconf.longPushThr,unit=unit)
+		result4 = client.write_register(2002,abusliconf.timeoutThr,unit=unit)
+	except:
+		print("Modbus error.")
+	client.close()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("command", type=str, help="command to perform. Allowed commands: upload, download, store, compare")
+parser.add_argument("address", type=int, help="device address")
+parser.add_argument("file", type=str, help="config file source/destination")
+args = parser.parse_args()
+unit = args.address
+abusliconf.filename = args.file
+if (args.command == "download"):
+	readConfs()
+	abusliconf.buttonConf=buttonConf
+	abusliconf.ioConf=IOcffromDevice
+	abusliconf.oConf=oConfFromDevice
+#	print(longpushThrFromDevice)
+	abusliconf.longPushThr=cmdRegisters[1]
+	abusliconf.timeoutThr=cmdRegisters[2]
+	abusliconf.writeConfToFile()
+	print ("Written to file: "+abusliconf.filename)
+
+	if(compare()):
+		print("Verification: pass")
+elif (args.command == "compare"):
+	if(compare()):
+		print("Verification: pass")
+elif (args.command == "upload"):
+	print("Please wait, this might take a couple of seconds..")
+	upload()
+	if(compare()):
+		print("Verification: pass")
+elif (args.command == "store"):
+	if(compare()):
+		print("Verification: pass")
+		store()
+	else:
+		print("Cannot write to EEPROM!")
+		
+		
+else:
+	
+#	blah = [0,5,3,6,5,2]
+#	print blah[0:4]
+	print("Invalid command. Allowed commands: upload, download, store, compare")

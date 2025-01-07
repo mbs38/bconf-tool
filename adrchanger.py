d0da2709b926c22d4029474cf5258f9fd4b6b218
Max Brüggemann <mail@maxbrueggemann.de>
2025-01-07 21:29:46 +0100

    Fix adrchanger

--- adrchanger.py
+++ adrchanger.py
@@ -1,74 +1,75 @@
-# Ugly code following!     
-
-import array
-import pymodbus
-import serial
+#!/usr/bin/env python3
 import argparse
-from pymodbus.pdu import ModbusRequest
-from pymodbus.client import ModbusSerialClient as ModbusClient #initialize a serial RTU client instance
-from pymodbus.transaction import ModbusRtuFramer
-
-#import logging
-#logging.basicConfig()
-#log = logging.getLogger()
-#log.setLevel(logging.DEBUG)
-
-baudrate = 38400
-parity = 'N'
-port = "/dev/ttyUSB0"
-
-global newAdr
-
-def probe():
-	try:
-		client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate, timeout=0.5)
-		connection = client.connect()
-		result0 = client.read_coils(2000,1,slave=unit)
-		client.close()
-	except:
-		print ("Serial error. Is "+port+" available?")
-		client.close()
-	else:
-		try:
-			result0.string
-		except:
-			client.close()
-			return True
-		else:
-			client.close()
-			print("Client unreachable!")
-			return False
+import asyncio
+import pymodbus
+import logging
+import pymodbus.client as ModbusClient
 
 
+newAdr = None
+unit = None
+client: ModbusClient.ModbusBaseClient
 
-def chAdr():
+async def run_async_simple_client():
+    """Run async client."""
+    global client
+    client = ModbusClient.AsyncModbusSerialClient(
+        "/dev/ttyUSB0",
+        timeout=0.1,
+        retries=0,
+        baudrate=38400,
+        bytesize=8,
+        parity="N",
+        stopbits=1,
+        reconnect_delay=0.1,
+        reconnect_delay_max=0
+    )
 
-	client = ModbusClient(method = "rtu", port = port, stopbits = 1, bytesize = 8, parity = parity, baudrate = baudrate)
-	try:
-		connection = client.connect()
-		adrReg = client.read_holding_registers(1000,1,slave=unit)
-		result = client.write_register(1000,newAdr,slave=unit)		
-		adrReg = client.read_holding_registers(1000,1,slave=unit)
-		if adrReg.registers[0] == newAdr:
-			print("Write: ok")
-			result = client.write_register(1001,16727,slave=unit)		
-		print("Store: ok")
-	except:
-		print("Failed!")
-	try:
-		adrReg = client.read_holding_registers(1000,1,slave=newAdr)
-		print("Verification: pass")
-		print("New address: "+str(newAdr))
-	except:
-		print("Verification failed!")
-	client.close()
+    client.set_max_no_responses(0xFFFFFFFF)
+    await client.connect()
+    assert client.connected
 
-parser = argparse.ArgumentParser()
+async def probe():
+    global client
+    try:
+        result0 = await client.read_coils(2000,count=1,slave=unit)
+    except Exception as e:
+        print(e)
+        print("Client not reachable, exitting.")
+        exit()
 
-parser.add_argument("address", type=int, help="device's current address")
-parser.add_argument("newAddress", type=int, help="new device address")
-args = parser.parse_args()
-unit = args.address
-newAdr = args.newAddress
-if probe():
-	chAdr()
+async def chAdr():
+    try:
+        adrReg = await client.read_holding_registers(1000,count=1,slave=unit)
+        result = await client.write_register(1000,newAdr,slave=unit)        
+        adrReg = await client.read_holding_registers(1000,count=1,slave=unit)
+        if adrReg.registers[0] == newAdr:
+            print("Write: ok")
+            result = await client.write_register(1001,16727,slave=unit)        
+        print("Store: ok")
+    except:
+        print("Failed!")
+    try:
+        adrReg = await client.read_holding_registers(1000,count=1,slave=newAdr)
+        print("Verification: pass")
+        print("New address: "+str(newAdr))
+    except:
+        print("Verification failed!")
+
+async def run():
+    logger = logging.getLogger()
+    logging.disable(logging.CRITICAL)
+    await run_async_simple_client()
+    await probe()
+    await chAdr()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("address", type=int, help="device's current address")
+    parser.add_argument("newAddress", type=int, help="new device address")
+    args = parser.parse_args()
+    unit = args.address
+    newAdr = args.newAddress
+    asyncio.run(
+        run()
+    )
